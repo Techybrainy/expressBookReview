fb27ee5b0207355c451c7a85acb97b046177a89c
Tech Dhruv <techdhruv1316@gmail.com>
2026-09-03 21:56:03 +0000

    Implement get all books route

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -6,14 +6,12 @@ const public_users = express.Router();
 
 
 public_users.post("/register", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    return res.status(200).json(books);
 });
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  return res.status(200).json(books);
 });
 
 // Get book details based on ISBN
