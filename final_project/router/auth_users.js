ec98a90c1cb40c801267b5d6318d5aef8afc0e47
Tech Dhruv <techdhruv1316@gmail.com>
2026-09-03 22:52:07 +0000

    Add delete book review

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -37,6 +37,15 @@ regd_users.post("/login", (req,res) => {
     });
 });
 
+regd_users.delete("/auth/review/:isbn", (req, res) => {
+    const isbn = req.params.isbn;
+    const username = req.user.username;
+    if (!books[isbn]) return res.status(404).json({message: "Book not found"});
+    if (!books[isbn].reviews[username]) return res.status(404).json({message: "Review not found"});
+    delete books[isbn].reviews[username];
+    return res.status(200).json({message: "Review deleted successfully"});
+});
+
 regd_users.put("/auth/review/:isbn", (req, res) => {
     const isbn = req.params.isbn;
     const username = req.user.username;
